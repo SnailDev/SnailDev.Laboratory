db61a06c8f12c8a4fab033cfbf17b5d275b79870
zhaoming <zhaoming@mallcoo.cn>
2017-09-20 18:07:40 +0800

    class default method also could be overrided

--- src/python/class_default.py
+++ src/python/class_default.py
@@ -21,8 +21,8 @@ class Vector:
       self.a = a
       self.b = b
  
-   def __str__(self):
-      return 'Vector (%d, %d)' % (self.a, self.b)
+#    def __str__(self):
+#       return 'Vector (%d, %d)' % (self.a, self.b)
    
    def __add__(self,other):
       return Vector(self.a + other.a, self.b + other.b)
