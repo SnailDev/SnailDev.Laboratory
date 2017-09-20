eb8c7e0f511e9f13f42eeb4cedd4a65da1e540f6
zhaoming <zhaoming@mallcoo.cn>
2017-09-20 18:09:19 +0800

    Operator Overloading

--- src/python/class_default.py
+++ src/python/class_default.py
@@ -23,7 +23,8 @@ class Vector:
  
 #    def __str__(self):
 #       return 'Vector (%d, %d)' % (self.a, self.b)
-   
+
+   #Operator Overloading  
    def __add__(self,other):
       return Vector(self.a + other.a, self.b + other.b)
  
